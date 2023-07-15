952a834bd81b824e9b6c5a70d18a3c71ccb3ad0b
hamonia777 <woong010207@gmail.com>
2023-07-15 15:12:55 +0900

    feat: 회원가입,이메일 중복 확인,닉네임 중복확인 구현

--- src/main/java/com/example/inobao/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/example/inobao/domain/user/dto/UserResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.inobao.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserResponseDto {
+    private String msg;
+    private int status;
+//    public UserResponseDto(String msg,int status){
+//        this.msg=msg;
+//        this.status=status;
+//    }
 }
--- src/main/java/com/example/inobao/domain/user/entity/User.java
+++ src/main/java/com/example/inobao/domain/user/entity/User.java
@@ -1,4 +1,41 @@
 package com.example.inobao.domain.user.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false, unique = true)
+   @Email
+   private String email;
+
+   @Column(nullable = false)
+   private String password;
+
+   @Column(nullable = false)
+   private  String nickname;
+
+   @Column(nullable = false)
+   @Enumerated(value = EnumType.STRING)    // Enum값 그대로 db에 저장
+   private UserRoleEnum role;
+
+   @Builder
+   private User(String email, String password, String nickname,UserRoleEnum role) {
+      this.email = email;
+      this.password = password;
+      this.nickname = nickname;
+      this.role=role;
+   }
 }
