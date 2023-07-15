6fe27a558d6b00e52616d74a49925c7dd2cc943a
Eulga <kimyeoun10@gmail.com>
2023-07-15 11:28:30 +0900

    feat: 엔티티 클래스 작성
    ERD기반의 엔티티 클래스 생성 후 연관관계 설정

--- src/main/java/com/example/inobao/domain/post/entity/Post.java
+++ src/main/java/com/example/inobao/domain/post/entity/Post.java
@@ -1,4 +1,58 @@
 package com.example.inobao.domain.post.entity;
 
+import com.example.inobao.domain.comment.entity.Comment;
+import com.example.inobao.domain.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import static jakarta.persistence.FetchType.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    private User user;
+
+    @NotBlank(message = "게시글 공백 불가")
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "post", fetch = LAZY, cascade = CascadeType.REMOVE)
+    private List<Comment> commentList;
+
+    @Column(updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public Post(User user, String content) {
+        this.user = user;
+        this.content = content;
+    }
+
+    public void modifyPost(String content) {
+        this.content = content;
+    }
 }
