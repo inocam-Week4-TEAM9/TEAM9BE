3f96acb0155a95ea719e33937472da770f187d3f
wkdtmddus <jangsy1207@gmail.com>
2023-07-17 11:22:10 +0900

    feat: post

--- src/main/java/com/example/inobao/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/example/inobao/domain/post/dto/PostResponseDto.java
@@ -1,4 +1,29 @@
 package com.example.inobao.domain.post.dto;
 
+import com.example.inobao.domain.comment.dto.CommentResponseDto;
+import com.example.inobao.domain.post.entity.Post;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
 public class PostResponseDto {
+    private final Long id;
+    private final String nickname;
+    private final String contents;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
+
+    private List<CommentResponseDto> commentList;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.nickname = post.getUser().getNickname();
+        this.contents = post.getContent();
+        this.createdDate = post.getCreateDate();
+        this.modifiedDate = post.getModifiedDate();
+
+        this.commentList = (post.getCommentList() == null) ? null : post.getCommentList().stream().map(CommentResponseDto::new).toList();
+    }
 }
