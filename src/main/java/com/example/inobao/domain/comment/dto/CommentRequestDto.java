70da77d9983c00c8b66a0f207736314a97cc44cd
wkdtmddus <jangsy1207@gmail.com>
2023-07-17 11:22:02 +0900

    feat: comment

--- src/main/java/com/example/inobao/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/inobao/domain/comment/dto/CommentRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.inobao.domain.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+    @NotBlank(message = "게시글 공백 불가")
+    private final String content;
+
+    public CommentRequestDto(String content) {
+        this.content = content;
+    }
 }
