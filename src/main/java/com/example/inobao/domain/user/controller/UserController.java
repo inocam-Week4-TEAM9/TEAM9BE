a586577f6de44f4625f560a35ca4c9dbeea69845
Eulga <kimyeoun10@gmail.com>
2023-07-20 12:02:19 +0900

    fix: 이메일, 닉네임 체크 이슈 해결
    UserController
    - @RequestBody EmailAndNicknameCheckDto로 받아 쪼개서 처리

--- src/main/java/com/example/inobao/domain/user/controller/UserController.java
+++ src/main/java/com/example/inobao/domain/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.inobao.domain.user.controller;
 
+import com.example.inobao.domain.user.dto.EmailAndNicknameCheckDto;
 import com.example.inobao.domain.user.dto.UserRequestDto;
 import com.example.inobao.domain.user.dto.UserResponseDto;
 import com.example.inobao.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,12 +25,14 @@ public class UserController {
     //중복되면 false
     //중복안되면 true
     @PostMapping("/auth/email")
-    public Boolean checkeMail(@RequestParam String email) {
+    public Boolean checkeMail(@RequestBody EmailAndNicknameCheckDto body) {
+        String email = body.getEmail();
         return !userService.checkEmail(email);
     }
 
     @PostMapping("/auth/nickname")
-    public Boolean checkNickname(@RequestParam String nickname) {
+    public Boolean checkNickname(@RequestBody EmailAndNicknameCheckDto body) {
+        String nickname = body.getNickname();
         return !userService.checkNickname(nickname);
     }
 
---
+++ src/main/java/com/example/inobao/domain/user/dto/EmailAndNicknameCheckDto.java
@@ -0,0 +1,9 @@
+package com.example.inobao.domain.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmailAndNicknameCheckDto {
+    private String email;
+    private String nickname;
+}
